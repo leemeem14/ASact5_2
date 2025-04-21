0405fe7563ec1b60c13d049c99d9f999abd4b8fc
leemeem14 <leemeem19@naver.com>
2025-04-21 14:23:28 +0900

    Initial commit

---
+++ app/src/main/java/kr/ac/kopo/act5_2/MainActivity2.java
@@ -0,0 +1,38 @@
+package kr.ac.kopo.act5_2;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+    EditText edit;
+    Button btn;
+    TextView result;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        edit = findViewById(R.id.edit);
+        btn = findViewById(R.id.btn);
+        result = findViewById(R.id.result);
+
+        btn.setOnClickListener(v -> {
+            String str = edit.getText().toString();
+            result.setText(str);
+        });
+    }
+}
